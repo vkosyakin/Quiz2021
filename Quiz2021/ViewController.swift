ff778278ba99e412f8453add54f308330ead5fcf
Valeriy Kosyakin <valeradzu@mail.ru>
2021-08-11 10:23:45 +0300

    Initial Commit

---
+++ Quiz2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz2021
+//
+//  Created by Валера on 11.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
