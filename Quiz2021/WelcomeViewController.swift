546c3e2ef7f6b612064a43588ca06a6978ff06d5
Valeriy Kosyakin <valeradzu@mail.ru>
2021-08-11 11:48:29 +0300

    Designed welcome screen

--- Quiz2021/WelcomeViewController.swift
+++ Quiz2021/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  Quiz2021
 //
 //  Created by Валера on 11.08.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
